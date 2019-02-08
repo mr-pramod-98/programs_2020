3d4e276b4779bd8bebd2f7fa2f568ffc32943585
mr-pramod-98 <43513968+mr-pramod-98@users.noreply.github.com>
2019-02-08 23:25:09 +0530

    Update bsort_functionPointer.c

--- bsort_functionPointer.c
+++ bsort_functionPointer.c
@@ -2,25 +2,27 @@
 #include<string.h>
 #include<stdlib.h>
  
-void swaping(const void *a,const void *b) 
+int compare(const void *a,const void *b) 
 { 
-    char temp[10]; 
-    strcpy((char*)temp,(char*)a); 
-    strcpy((char*)a,(char*)b);
-    strcpy((char*)b,(char*)temp);
+    return strcmp((char*)a,(char*)b);
 } 
 
-void BubbleSort(char a[5][10],int n,int size,void(*swap)(char *s1,char *s2))
+void BubbleSort(void *const a,int n,int size,int(*comp)(char *s1,char *s2))
  {
-  int i,j;
-  for(i=0;i<n;i++)
-  {
-   for(j=0;j<n;j++)
-   {
-    if(strcmp(a[i],a[j])>=0)
-       swaping(&a[i],&a[j]);
-   }
-  }
+    int i,j; 
+    char temp[10]; 
+    for (j=0; j<(n*size)-10; j=j+size) 
+    { 
+        for (i=j+10; i<(n*size); i=i+size) 
+        { 
+            if (compare((char*)(a+j),(char*)(a+i)) > 0) 
+            { 
+                strcpy(temp,(char*)(a+j)); 
+                strcpy((char*)(a+j),(char*)(a+i)); 
+                strcpy((char*)(a+i), temp); 
+            } 
+        } 
+    } 
  }
 
 main()
@@ -30,7 +32,7 @@ main()
  printf("before sorting\n");
  for(i=0;i<5;i++)
     puts(a[i]);
- BubbleSort(a,5,sizeof(a[0]),swaping);
+ BubbleSort(a,5,sizeof(a[0]),compare);
  printf("after sorting\n");
  for(i=0;i<5;i++)
    puts(a[i]);
